08e5bc2d17477f16cf6e59bdeeb5b1358764d2d9
Dan Kogai <dankogai+github@gmail.com>
2016-01-08 13:03:36 +0900

    update test/01-roundtrip.js

--- test/01-roundtrip.js
+++ test/01-roundtrip.js
@@ -1,5 +1,5 @@
 /*
- * $Id: 01-roundtrip.js,v 0.1 2016/01/08 03:58:24 dankogai Exp dankogai $
+ * $Id: 01-roundtrip.js,v 0.2 2016/01/08 04:03:25 dankogai Exp dankogai $
  *
  * use mocha to test me
  * http://visionmedia.github.com/mocha/
@@ -18,5 +18,6 @@ describe('Roundtrip', function () {
     for (var d = 1, f = 1; d < 2.0; f /= 2, d += f) {
         var s = d.toHexString();
         it('('+d+').toHexString() == "'+s+'"', is(s, d.toHexString()));
+        it('parseHexFloat("'+s+'") == '+d, is(parseHexFloat(s), d ));
     }
 });
